3a9cf67781727f06f414c670debc267dd6040396
Peredreev.Nikita <nik.peredreev@mail.ru>
2024-03-23 18:00:06 +0400

    запушил тасик 1 и 2

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,22 @@
-void setup() {
+long micrs[5] = {0, 0, 0, 0, 0};
+long times[5] = {10000, 1000, 500, 100, 50}; //10мс 1мс 500мкс 100мкс 50мкс
+int lamps[5] = {LOW, LOW, LOW, LOW, LOW};
+int pins[5] = {3, 5, 6, 9, 10};
 
+void setup() {
+  for (int i = 0; i < 5; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop() {
+  unsigned long micr_cur = micros();
 
+  for (int i = 0; i < 5; i++) {
+    if (micr_cur - micrs[i] >= times[i]) {
+      micrs[i] = micr_cur;
+      lamps[i] = (lamps[i] == LOW) ? HIGH : LOW;
+      digitalWrite(pins[i], lamps[i]);
+    }
+  }
 }
--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,29 @@
-void setup() {
+#include <TaskManagerIO.h>
 
+long times[5] = {10000, 1000, 500, 100, 50}; // 10мс 1мс 500мкс 100мкс 50мкс
+int* lamps = new int[5];
+int pins[5] = {3, 5, 6, 9, 10};
+taskid_t taskIds[5];
+
+void toggleLamp(int index) {
+  if (lamps[index] == LOW) {
+    lamps[index] = HIGH;
+  } else {
+    lamps[index] = LOW;
+  }
+  digitalWrite(pins[index], lamps[index]);
+}
+
+void setup() {
+  for (int i = 0; i < 5; i++) {
+    lamps[i] = LOW;
+    pinMode(pins[i], OUTPUT);
+    taskIds[i] = taskManager.scheduleFixedRate(times[i], [i] {
+      toggleLamp(i);
+    }, TIME_MICROS);
+  }
 }
 
 void loop() {
-
+  taskManager.runLoop();
 }
