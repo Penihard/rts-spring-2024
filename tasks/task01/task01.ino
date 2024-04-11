c31c15c57c6f3dd774e32825ee3956b33dcebb86
Peredreev.Nikita <nik.peredreev@mail.ru>
2024-04-12 00:21:18 +0300

    запушил обновленные таски

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,22 +1,99 @@
-long micrs[5] = {0, 0, 0, 0, 0};
+long* micrs = new long[5];
 long times[5] = {10000, 1000, 500, 100, 50}; //10мс 1мс 500мкс 100мкс 50мкс
-int lamps[5] = {LOW, LOW, LOW, LOW, LOW};
+int* lamps = new int[5];
 int pins[5] = {3, 5, 6, 9, 10};
 
 void setup() {
-  for (int i = 0; i < 5; i++) {
+  // put your setup code here, to run once:
+  for (int i = 0; i < 5; i++) 
+  {
+    micrs[i] = 0;
+    lamps[i] = LOW;
     pinMode(pins[i], OUTPUT);
   }
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   unsigned long micr_cur = micros();
 
-  for (int i = 0; i < 5; i++) {
-    if (micr_cur - micrs[i] >= times[i]) {
-      micrs[i] = micr_cur;
-      lamps[i] = (lamps[i] == LOW) ? HIGH : LOW;
-      digitalWrite(pins[i], lamps[i]);
+  if (micr_cur - micrs[0] >= times[0]) 
+  {
+    micrs[0] = micr_cur;
+
+    if (lamps[0] == LOW) 
+    {
+      lamps[0] = HIGH;
     }
+    else 
+    {
+      lamps[0] = LOW;
+    }
+
+    digitalWrite(pins[0], lamps[0]);
+  }
+
+  if (micr_cur - micrs[1] >= times[1]) 
+  {
+    micrs[1] = micr_cur;
+
+    if (lamps[1] == LOW) 
+    {
+      lamps[1] = HIGH;
+    }
+    else 
+    {
+      lamps[1] = LOW;
+    }
+
+    digitalWrite(pins[1], lamps[1]);
+  }
+  
+  if (micr_cur - micrs[2] >= times[2]) 
+  {
+    micrs[2] = micr_cur;
+
+    if (lamps[2] == LOW) 
+    {
+      lamps[2] = HIGH;
+    }
+    else 
+    {
+      lamps[2] = LOW;
+    }
+
+    digitalWrite(pins[2], lamps[2]);
+  }
+
+  if (micr_cur - micrs[3] >= times[3]) 
+  {
+    micrs[3] = micr_cur;
+
+    if (lamps[3] == LOW)
+    {
+      lamps[3] = HIGH;
+    }
+    else 
+    {
+      lamps[3] = LOW;
+    }
+
+    digitalWrite(pins[3], lamps[3]);
+  }
+
+  if (micr_cur - micrs[4] >= times[4]) 
+  {
+    micrs[4] = micr_cur;
+
+    if (lamps[4] == LOW) 
+    {
+      lamps[4] = HIGH;
+    }
+    else 
+    {
+      lamps[4] = LOW;
+    }
+
+    digitalWrite(pins[4], lamps[4]);
   }
 }
--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,29 +1,91 @@
 #include <TaskManagerIO.h>
 
-long times[5] = {10000, 1000, 500, 100, 50}; // 10мс 1мс 500мкс 100мкс 50мкс
+long times[5] = {10000, 1000, 500, 100, 50}; //10мс 1мс 500мкс 100мкс 50мкс
 int* lamps = new int[5];
 int pins[5] = {3, 5, 6, 9, 10};
-taskid_t taskIds[5];
-
-void toggleLamp(int index) {
-  if (lamps[index] == LOW) {
-    lamps[index] = HIGH;
-  } else {
-    lamps[index] = LOW;
-  }
-  digitalWrite(pins[index], lamps[index]);
-}
 
 void setup() {
-  for (int i = 0; i < 5; i++) {
+  // put your setup code here, to run once:
+    for (int i = 0; i < 5; i++) 
+  {
     lamps[i] = LOW;
     pinMode(pins[i], OUTPUT);
-    taskIds[i] = taskManager.scheduleFixedRate(times[i], [i] {
-      toggleLamp(i);
-    }, TIME_MICROS);
   }
+
+  taskid_t taskId0 = taskManager.scheduleFixedRate(times[0], [] {
+      if (lamps[0] == LOW) 
+      {
+        lamps[0] = HIGH;
+      }
+      else {
+        lamps[0] = LOW;
+      }
+
+      digitalWrite(pins[0], lamps[0]);
+    }, TIME_MICROS);
+
+  taskid_t taskId1 = taskManager.scheduleFixedRate(times[1], [] {
+      if (lamps[1] == LOW) 
+      {
+        lamps[1] = HIGH;
+      }
+      else {
+        lamps[1] = LOW;
+      }
+
+      digitalWrite(pins[1], lamps[1]);
+    }, TIME_MICROS);
+
+  taskid_t taskId2 = taskManager.scheduleFixedRate(times[2], [] {
+      if (lamps[2] == LOW) 
+      {
+        lamps[2] = HIGH;
+      }
+      else {
+        lamps[2] = LOW;
+      }
+
+      digitalWrite(pins[2], lamps[2]);
+    }, TIME_MICROS);
+
+  taskid_t taskId3 = taskManager.scheduleFixedRate(times[3], [] {
+      if (lamps[3] == LOW) 
+      {
+        lamps[3] = HIGH;
+      }
+      else {
+        lamps[3] = LOW;
+      }
+
+      digitalWrite(pins[3], lamps[3]);
+    }, TIME_MICROS);
+
+  taskid_t taskId4 = taskManager.scheduleFixedRate(times[4], [] {
+      if (lamps[4] == LOW) 
+      {
+        lamps[4] = HIGH;
+      }
+      else {
+        lamps[4] = LOW;
+      }
+
+      digitalWrite(pins[4], lamps[4]);
+    }, TIME_MICROS);
+
+  taskid_t taskId5 = taskManager.scheduleFixedRate(times[5], [] {
+      if (lamps[5] == LOW) 
+      {
+        lamps[5] = HIGH;
+      }
+      else {
+        lamps[5] = LOW;
+      }
+
+      digitalWrite(pins[5], lamps[5]);
+    }, TIME_MICROS);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   taskManager.runLoop();
 }
